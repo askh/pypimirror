c7b8c0b3314e965721ab0338a9466ad535103096
Alexander Kharitonov <askh@askh.ru>
2022-04-05 09:46:37 +0300

    The password for the root user in the .env file.

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM python
 EXPOSE 3141
 RUN apt update && apt upgrade -y
 RUN useradd -d /home/devpisrv -m -s /bin/bash -U devpisrv
-RUN mkdir -p -m 750 /var/local/devpisrv && chown devpisrv:devpisrv /var/local/devpisrv
+RUN mkdir -p -m 750 /var/local/devpisrv && mkdir -m 750 /var/local/devpisrv/server && mkdir /var/local/devpisrv/install && chown -R devpisrv:devpisrv /var/local/devpisrv
+COPY --chown=devpisrv:devpisrv .env* /var/local/devpisrv/install/
 COPY requirements.txt /tmp/
 USER devpisrv:devpisrv
 RUN pip3 install -U pip
--- src/devpisrv.sh
+++ src/devpisrv.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-SERVER_DIR=/var/local/devpisrv
+DEFAULT_INSECURE_ROOT_PASSWORD="root"
+
+DATA_DIR=/var/local/devpisrv
+SERVER_DIR=${DATA_DIR}/server
+INSTALL_DIR=${DATA_DIR}/install
+ENV_FILE=${INSTALL_DIR}/.env
 
 if [[ ! -f "$SERVER_DIR/.nodeinfo" ]]
 then
-    devpi-init --serverdir "$SERVER_DIR"
+    if [[ -f "$ENV_FILE" ]]
+    then
+        source "$ENV_FILE"
+    fi
+    INSTALL_ROOT_PASSWORD=${ROOT_PASSWORD:-${DEFAULT_INSECURE_ROOT_PASSWORD}}
+    devpi-init --serverdir "$SERVER_DIR" --root-passwd "${INSTALL_ROOT_PASSWORD}"
+    rm "${INSTALL_DIR}/*"
 fi
 
-devpi-server --serverdir "$SERVER_DIR" --host 0.0.0.0
+devpi-server --serverdir "$SERVER_DIR" --host 0.0.0.0 --restrict-modify root
 
